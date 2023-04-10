c3068e4f82ec71060538514af3d6902c9cd6eef2
Raym0NT <konasr6@gmail.com>
2023-04-10 22:33:07 +0300

    style for header,hero,services and turnkey solutions withoud media request

--- js/index.js
+++ js/index.js
@@ -1,31 +1,37 @@
-// Services Accordion
-const accordions = document.querySelectorAll(".services__accordion");
-const initOpen = document
-  .querySelector(".services__accordion--open")
-  .querySelector(".services__accordion-content");
-const bgHeight = document.querySelector(".services__bg-item");
+// Services tabs
+const servicesTabs = () => {
+  const tabControlersList = document.querySelectorAll(".services__tab-btn");
+  const tabContentList = document.querySelectorAll(".services__tab-content");
+  let tabName;
+  const initTab = document.querySelector(
+    ".services__tab-content.services__tab--active"
+  );
+  initTab.style.maxHeight = `100%`;
 
-initOpen.style.maxHeight = `${initOpen.scrollHeight}px`;
-bgHeight.style.height = `${initOpen.scrollHeight}px`;
+  tabControlersList.forEach(item => {
+    item.addEventListener("click", e => {
+      tabControlersList.forEach(i => {
+        i.classList.remove("services__tab--active");
+      });
+      e.target.classList.add("services__tab--active");
 
-accordions.forEach(el => {
-  el.addEventListener("click", e => {
-    const self = e.currentTarget;
-    const btn = self.querySelector(".services__accordion-btn");
-    const content = self.querySelector(".services__accordion-content");
-
-    if (self.classList.contains("services__accordion--open")) return;
-
-    accordions.forEach(item => {
-      item.classList.remove("services__accordion--open");
-      item.querySelector(".services__accordion-content").style.maxHeight = null;
+      tabName = e.target.getAttribute("data-tab-name");
+      tabContentChange(tabName);
     });
-
-    self.classList.add("services__accordion--open");
-    content.style.maxHeight = `${content.scrollHeight}px`;
-    bgHeight.style.height = `${content.scrollHeight}px`;
   });
-});
+
+  const tabContentChange = tabName => {
+    tabContentList.forEach(i => {
+      i.classList.remove("services__tab--active");
+      i.style.maxHeight = `0px`;
+    });
+    tabContentList[+tabName - 1].classList.add("services__tab--active");
+    tabContentList[+tabName - 1].style.maxHeight = `${
+      tabContentList[+tabName - 1].scrollHeight
+    }px`;
+  };
+};
+servicesTabs();
 
 // Turnkey solutions slider
 const turnkeySolutionsSlider = new Swiper(".turnkey-solutions__slider", {
@@ -85,3 +91,32 @@ const articlesSlider = new Swiper(".articles__slider", {
     },
   },
 });
+/* const accordions = document.querySelectorAll(".services__accordion");
+const initOpen = document
+  .querySelector(".services__accordion--open")
+  .querySelector(".services__accordion-content");
+const bgHeight = document.querySelector(".services__bg-item");
+
+initOpen.style.maxHeight = `${initOpen.scrollHeight}px`;
+bgHeight.style.height = `${initOpen.scrollHeight}px`;
+
+accordions.forEach(el => {
+  el.addEventListener("click", e => {
+    const self = e.currentTarget;
+    const btn = self.querySelector(".services__accordion-btn");
+    const content = self.querySelector(".services__accordion-content");
+    
+    if (self.classList.contains("services__accordion--open")) return;
+
+    accordions.forEach(item => {
+      item.classList.remove("services__accordion--open");
+      item.querySelector(".services__accordion-content").style.maxHeight = null;
+    });
+
+    self.classList.add("services__accordion--open");
+    content.style.maxHeight = `${content.scrollHeight}px`;
+    bgHeight.style.height = `${content.scrollHeight}px`;
+
+    document.querySelector("services__list-content-desktop").append();
+  });
+});*/
