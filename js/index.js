302d9f41348ddf10f48dfc4bc8f58efed6882cd6
Raym0NT <konasr6@gmail.com>
2023-04-11 21:00:49 +0300

    reviews, about-company,advantages, process work

--- js/index.js
+++ js/index.js
@@ -65,9 +65,8 @@ const casesSlider = new Swiper(".cases__slider", {
 
 // Reviews Slider
 const reviewsSlider = new Swiper(".reviews__slider", {
-  slidesPerView: 3,
+  slidesPerView: "auto",
   spaceBetween: 20,
-
   navigation: {
     nextEl: ".reviews__slider-arrow--next",
     prevEl: ".reviews__slider-arrow--prev",
@@ -89,6 +88,49 @@ const articlesSlider = new Swiper(".articles__slider", {
     renderBullet: function (index, className) {
       return `<span class="${className}">${index + 1}</span>`;
     },
+    // renderCustom: function (swiper, current, total) {
+    //   let paginationHtml = "";
+    //   const maxVisible = 3; // максимальное количество видимых элементов
+
+    //   // добавляем первый элемент
+    //   paginationHtml +=
+    //     '<span class="swiper-pagination-bullet">' + 1 + "</span>";
+
+    //   // добавляем обрезанные числа, если их нужно показать
+    //   if (total > maxVisible) {
+    //     const start = current - 1;
+    //     const end = current + 1;
+    //     if (current < 2) {
+    //       end += 2 - current;
+    //     } else if (current > total - 2) {
+    //       start -= current - (total - 3);
+    //     }
+    //     if (start > 1) {
+    //       paginationHtml += '<span class="swiper-pagination-bullet">..</span>';
+    //     }
+    //     for (let i = start; i <= end; i++) {
+    //       if (i > 1 && i < total) {
+    //         paginationHtml +=
+    //           '<span class="swiper-pagination-bullet">' + i + "</span>";
+    //       }
+    //     }
+    //     if (end < total) {
+    //       paginationHtml += '<span class="swiper-pagination-bullet">..</span>';
+    //     }
+    //   } else {
+    //     // добавляем остальные элементы
+    //     for (let i = 2; i <= total - 1; i++) {
+    //       paginationHtml +=
+    //         '<span class="swiper-pagination-bullet">' + i + "</span>";
+    //     }
+    //   }
+
+    //   // добавляем последний элемент
+    //   paginationHtml +=
+    //     '<span class="swiper-pagination-bullet">' + total + "</span>";
+
+    //   return paginationHtml;
+    // },
   },
 });
 /* const accordions = document.querySelectorAll(".services__accordion");
