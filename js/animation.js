184df4f34605e40a9b7d30f27ba795599e9e87e1
Raym0NT <konasr6@gmail.com>
2023-04-12 22:46:04 +0300

    half of page animation

---
+++ js/animation.js
@@ -0,0 +1,5 @@
+AOS.init({
+  delay: 300, // values from 0 to 3000, with step 50ms
+  duration: 600, // values from 0 to 3000, with step 50ms
+  once: true, // whether animation should happen only once - while scrolling down
+});
