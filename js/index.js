e0e0df4fbd0c50a4f97919633c5415f4571080a8
Raym0NT <konasr6@gmail.com>
2023-04-13 15:08:39 +0300

    fixs and adaptive for header, hero, services

--- js/index.js
+++ js/index.js
@@ -1,43 +1,112 @@
 // FancyBox Init
 Fancybox.bind('[data-fancybox="gallery"]', {});
-Fancybox.bind("[data-fancybox='reviews']", {});
+Fancybox.bind("[data-fancybox='reviews-gallery']", {});
+
+// Header dropdown
+const headerDropdownBtn = document.querySelector(".header__dropdown-btn");
+const headerDropdownList = document.querySelector(".header__list");
+headerDropdownBtn.addEventListener("click", () => {
+  if (headerDropdownBtn.classList.contains("header__dropdown--active")) {
+    headerDropdownBtn.classList.remove("header__dropdown--active");
+    headerDropdownList.classList.remove("header__dropdown--active");
+  } else {
+    headerDropdownBtn.classList.add("header__dropdown--active");
+    headerDropdownList.classList.add("header__dropdown--active");
+  }
+});
+
+// Hero slider
+const swiper = new Swiper(".hero__slider", {
+  slidesPerView: 1,
+  loop: true,
+  centeredSlides: true,
+
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+
+  simulateTouch: false,
+
+  autoplay: {
+    delay: 2000,
+
+    stopOnLastSlide: false,
+
+    disableOnInteraction: false,
+  },
+});
 
 // Services tabs
-document.addEventListener("DOMContentLoaded", () => {});
-const servicesTabs = () => {
-  const tabControlersList = document.querySelectorAll(".services__tab-btn");
-  const tabContentList = document.querySelectorAll(".services__tab-content");
-  let tabName;
-  const initTab = document.querySelector(
-    ".services__tab-content.services__tab--active"
-  );
+document.addEventListener("DOMContentLoaded", e => {
+  console.log(e);
+  const servicesTabs = () => {
+    const tabControlersList = document.querySelectorAll(".services__tab-btn");
+    const tabContentList = document.querySelectorAll(".services__tab-content");
+    let tabName;
+    const initTab = document.querySelector(
+      ".services__tab-content.services__tab--active"
+    );
 
-  initTab.style.maxHeight = initTab.style.maxHeight = `325px`;
+    initTab.style.maxHeight = `${initTab.scrollHeight + 30}px`;
 
-  tabControlersList.forEach(item => {
-    item.addEventListener("click", e => {
-      tabControlersList.forEach(i => {
-        i.classList.remove("services__tab--active");
+    tabControlersList.forEach(item => {
+      item.addEventListener("click", e => {
+        tabControlersList.forEach(i => {
+          i.classList.remove("services__tab--active");
+        });
+        e.target.classList.add("services__tab--active");
+
+        tabName = e.target.getAttribute("data-tab-name");
+        tabContentChange(tabName);
       });
-      e.target.classList.add("services__tab--active");
+    });
 
-      tabName = e.target.getAttribute("data-tab-name");
-      tabContentChange(tabName);
+    const tabContentChange = tabName => {
+      tabContentList.forEach(i => {
+        i.classList.remove("services__tab--active");
+        i.style.maxHeight = `0px`;
+      });
+      tabContentList[+tabName - 1].classList.add("services__tab--active");
+      tabContentList[+tabName - 1].style.maxHeight = `${
+        tabContentList[+tabName - 1].scrollHeight
+      }px`;
+    };
+  };
+  servicesTabs();
+});
+
+// Services accordion for mobile
+const servicesAccordion = () => {
+  const accordions = document.querySelectorAll(".services__accordion");
+  const initOpen = document
+    .querySelector(".services__accordion--open")
+    .querySelector(".services__accordion-content");
+
+  setTimeout(() => {
+    initOpen.style.maxHeight = `${initOpen.scrollHeight}px`;
+  }, 1000);
+
+  accordions.forEach(el => {
+    el.addEventListener("click", e => {
+      const self = e.currentTarget;
+      const btn = self.querySelector(".services__accordion-btn");
+      const content = self.querySelector(".services__accordion-content");
+
+      if (self.classList.contains("services__accordion--open")) return;
+
+      accordions.forEach(item => {
+        item.classList.remove("services__accordion--open");
+        item.querySelector(".services__accordion-content").style.maxHeight =
+          null;
+      });
+
+      self.classList.add("services__accordion--open");
+      content.style.maxHeight = `${content.scrollHeight}px`;
     });
   });
-
-  const tabContentChange = tabName => {
-    tabContentList.forEach(i => {
-      i.classList.remove("services__tab--active");
-      i.style.maxHeight = `0px`;
-    });
-    tabContentList[+tabName - 1].classList.add("services__tab--active");
-    tabContentList[+tabName - 1].style.maxHeight = `${
-      tabContentList[+tabName - 1].scrollHeight
-    }px`;
-  };
 };
-servicesTabs();
+servicesAccordion();
 
 // Turnkey solutions slider
 const turnkeySolutionsSlider = new Swiper(".turnkey-solutions__slider", {
@@ -139,32 +208,15 @@ const articlesSlider = new Swiper(".articles__slider", {
     // },
   },
 });
-/* const accordions = document.querySelectorAll(".services__accordion");
-const initOpen = document
-  .querySelector(".services__accordion--open")
-  .querySelector(".services__accordion-content");
-const bgHeight = document.querySelector(".services__bg-item");
 
-initOpen.style.maxHeight = `${initOpen.scrollHeight}px`;
-bgHeight.style.height = `${initOpen.scrollHeight}px`;
-
-accordions.forEach(el => {
-  el.addEventListener("click", e => {
-    const self = e.currentTarget;
-    const btn = self.querySelector(".services__accordion-btn");
-    const content = self.querySelector(".services__accordion-content");
-    
-    if (self.classList.contains("services__accordion--open")) return;
-
-    accordions.forEach(item => {
-      item.classList.remove("services__accordion--open");
-      item.querySelector(".services__accordion-content").style.maxHeight = null;
-    });
-
-    self.classList.add("services__accordion--open");
-    content.style.maxHeight = `${content.scrollHeight}px`;
-    bgHeight.style.height = `${content.scrollHeight}px`;
-
-    document.querySelector("services__list-content-desktop").append();
+// Rating stars
+const ratingItemsList = document.querySelectorAll(".popup-feedback__star");
+ratingItemsList.forEach(star => {
+  star.addEventListener("click", e => {
+    console.log(
+      star.parentNode.dataset.reviewTotalValue,
+      star.dataset.reviewItemValue
+    );
+    star.parentNode.dataset.reviewTotalValue = star.dataset.reviewItemValue;
   });
-});*/
+});
