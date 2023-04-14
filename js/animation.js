b7925e675c0ede0c11fa97a2e6fec415bfdb3c3b
Raym0NT <konasr6@gmail.com>
2023-04-14 17:59:22 +0300

    scroll animation

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,6 @@
+// AOS
 AOS.init({
-  delay: 300, // values from 0 to 3000, with step 50ms
-  duration: 600, // values from 0 to 3000, with step 50ms
+  delay: 250, // values from 0 to 3000, with step 50ms
+  duration: 1250, // values from 0 to 3000, with step 50ms
   once: true, // whether animation should happen only once - while scrolling down
 });
