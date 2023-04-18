eb49e7c9acded3fdba17829941e71653b7bf3785
Raym0NT <konasr6@gmail.com>
2023-04-18 14:14:34 +0300

    parallax fixs

--- js/parallax.js
+++ js/parallax.js
@@ -1,42 +1,43 @@
 const parallaxContainer = document.querySelector(".gallery");
 const parallaxItems = document.querySelectorAll(".gallery__item");
+const parallaxItemsImage = document.querySelectorAll(".gallery__item-image");
 
 // // Кф - чем меньше тем больше будет паралакс
-const coefficient = 5;
-const speed = 0.1;
+// const coefficient = 20;
+// const speed = 0.1;
 
-let posX = 0,
-  posY = 0;
-let coordXprocent = 0,
-  coordYprocent = 0;
+// let posX = 0,
+//   posY = 0;
+// let coordXprocent = 0,
+//   coordYprocent = 0;
 
-// // function setMouseParallaxStyle() {
-// //   const distX = coordXprocent - posX;
-// //   const distY = coordYprocent - posY;
+// function setMouseParallaxStyle() {
+//   const distX = coordXprocent - posX;
+//   const distY = coordYprocent - posY;
 
-// //   posX = posX + distX * speed;
-// //   posY = posY + distY * speed;
+//   posX = posX + distX * speed;
+//   posY = posY + distY * speed;
 
-// //   parallaxItems.forEach(item => {
-// //     item.style.cssText = `transform: translate(${posX / coefficient}%, ${
-// //       posY / coefficient
-// //     }%)`;
-// //   });
+//   parallaxItems.forEach(item => {
+//     item.style.cssText = `transform: translate(${posX / coefficient}%, ${
+//       posY / coefficient
+//     }%)`;
+//   });
 
-// //   requestAnimationFrame(setMouseParallaxStyle);
-// // }
-// // setMouseParallaxStyle();
+//   requestAnimationFrame(setMouseParallaxStyle);
+// }
+// setMouseParallaxStyle();
 
-// // parallaxContainer.addEventListener("mousemove", e => {
-// //   const parallaxWidth = parallaxContainer.offsetWidth;
-// //   const parallaxHeight = parallaxContainer.offsetHeight;
+// parallaxContainer.addEventListener("mousemove", e => {
+//   const parallaxWidth = parallaxContainer.offsetWidth;
+//   const parallaxHeight = parallaxContainer.offsetHeight;
 
-// //   const coordX = e.pageX - parallaxWidth / 2;
-// //   const coordY = e.pageY - parallaxHeight / 2;
+//   const coordX = e.pageX - parallaxWidth / 2;
+//   const coordY = e.pageY - parallaxHeight / 2;
 
-// //   coordXprocent = (coordX / parallaxWidth) * 100;
-// //   coordYprocent = (coordY / parallaxHeight) * 100;
-// // });
+//   coordXprocent = (coordX / parallaxWidth) * 100;
+//   coordYprocent = (coordY / parallaxHeight) * 100;
+// });
 
 // // Паралакс при скроле
 let thresholdSets = [];
@@ -50,26 +51,26 @@ const callback = (entries, observer) => {
 };
 
 const setParallaxItemsStyle = scrollTopProcent => {
-  parallaxItems[0].style.cssText = `transform: translate(0, ${
-    scrollTopProcent / 35
+  parallaxItemsImage[0].style.cssText = `transform: translate(0, ${
+    scrollTopProcent / 30
   }%)`;
-  parallaxItems[1].style.cssText = `transform: translate(0, ${
-    scrollTopProcent / 15
+  parallaxItemsImage[1].style.cssText = `transform: translate(0, ${
+    scrollTopProcent / 14
   }%)`;
-  parallaxItems[2].style.cssText = `transform: translate(0, ${
+  parallaxItemsImage[2].style.cssText = `transform: translate(0, ${
+    scrollTopProcent / 30
+  }%)`;
+  parallaxItemsImage[3].style.cssText = `transform: translate(0, ${
+    scrollTopProcent / 18
+  }%)`;
+  parallaxItemsImage[4].style.cssText = `transform: translate(0, ${
+    scrollTopProcent / 30
+  }%)`;
+  parallaxItemsImage[5].style.cssText = `transform: translate(0,${
+    scrollTopProcent / 40
+  }%)`;
+  parallaxItemsImage[6].style.cssText = `transform: translate(0, ${
     scrollTopProcent / 25
-  }%)`;
-  parallaxItems[3].style.cssText = `transform: translate(0, ${
-    scrollTopProcent / 20
-  }%)`;
-  parallaxItems[4].style.cssText = `transform: translate(0, ${
-    scrollTopProcent / 25
-  }%)`;
-  parallaxItems[5].style.cssText = `transform: translate(0,${
-    scrollTopProcent / 25
-  }%)`;
-  parallaxItems[6].style.cssText = `transform: translate(0, ${
-    scrollTopProcent / 20
   }%)`;
 };
 
